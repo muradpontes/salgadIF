2c2af88b8712303cbf871db95b5b69fa518cec99
murad <muradsp20@gmail.com>
2023-06-06 11:36:37 -0300

    update init_db.py

--- init_db.py
+++ init_db.py
@@ -2,12 +2,12 @@ import sqlite3
 
 connection = sqlite3.connect('banco.db')
 
-
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
+# superuser
 cur.execute("INSERT INTO usuario (nome, senha) VALUES (?, ?)",
             ('admin', 'admin123')
             )
